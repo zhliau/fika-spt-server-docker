80093fb17ee1311c0b42e833cbacd64acb4a9463
ZHL <45361542+zhliau@users.noreply.github.com>
2024-12-24 22:09:09 -0500

    Update to SPT 3.10.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ gid=${GID:-1000}
 backup_dir_name=${BACKUP_DIR:-backups}
 backup_dir=$mounted_dir/$backup_dir_name
 
-spt_version=${SPT_VERSION:-3.10.3}
+spt_version=${SPT_VERSION:-3.10.4}
 spt_backup_dir=$backup_dir/spt/$(date +%Y%m%dT%H%M)
 spt_data_dir=$mounted_dir/SPT_Data
 spt_core_config=$spt_data_dir/Server/configs/core.json
