a83c1a180218f1a4df2d4b0e7a997838c89cccc8
jonpas <jonpas33@gmail.com>
2025-12-28 07:24:15 +0100

    Update to SPT 4.0.10 (#57)

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ gid=${GID:-1000}
 backup_dir_name=${BACKUP_DIR:-backups}
 backup_dir=$mounted_dir/$backup_dir_name
 
-spt_version=${SPT_VERSION:-4.0.9-40087-ba49b00}
+spt_version=${SPT_VERSION:-4.0.10-40087-1ba317d}
 spt_version=$(echo $spt_version | cut -d '-' -f 1)
 spt_backup_dir=$backup_dir/spt/$(date +%Y%m%dT%H%M)
 # if force spt version, ignore all version checks and disable user folder backup
